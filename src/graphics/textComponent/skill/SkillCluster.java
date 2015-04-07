46fabc4a9540f9c1081e93c54468134183da36ad
Anton Milenin <anton.milenin@gmail.com>
2015-04-07 16:10:01 +0300

    jcombobox will not show same values in SkillCluster and sorted now

--- src/graphics/textComponent/skill/SkillCluster.java
+++ src/graphics/textComponent/skill/SkillCluster.java
@@ -8,52 +8,75 @@ import mechanics.character.GameCharacter;
 
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
+
+import javax.swing.DefaultComboBoxModel;
 
 /**
- * User: Рома
- * Date: 02.04.2015
- * Time: 5:29
+ * User: Рома Date: 02.04.2015 Time: 5:29
  */
 public class SkillCluster extends ChoiceCluster {
 
-    private SkillBase mySkillBase;
-    private Map<String, Skill> nameToSkill = new HashMap<String, Skill>();
-    private Skill mySkill;
-    private GameCharacter myCharacter;
+	private SkillBase mySkillBase;
+	private static Map<String, Skill> nameToSkill;
+	private static Vector<String> avaibleSkills;
+	private Skill mySkill;
+	private GameCharacter myCharacter;
 
-    public SkillCluster(int x, int y, int width, int height, String value, GameCharacter character) {
-        super(x, y, width, height, value);
-        myCharacter = character;
-        mySkillBase = character.getSkillBase();
+	public SkillCluster(int x, int y, int width, int height, String value, GameCharacter character) {
+		super(x, y, width, height, value);
+		myCharacter = character;
+		mySkillBase = character.getSkillBase();
 
-        for (Skill skill: mySkillBase.getSkills()) {
-            add(skill.getName());
-            nameToSkill.put(skill.getName(), skill);
-        }
+		// Everything runs in one thread so this is safe
+		if (nameToSkill == null) {
+			nameToSkill = new HashMap<>();
+			avaibleSkills = new Vector<>();
+			avaibleSkills.add(Skill.DEFAULT_ID);
+			for (Skill skill : mySkillBase.getSkills()) {
+				avaibleSkills.add(skill.getName());
+				nameToSkill.put(skill.getName(), skill);
+			}
+			Collections.sort(avaibleSkills);
+		}
+		choice.setModel(new DefaultComboBoxModel(avaibleSkills));
 
-        choice.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                String name = (String) e.getItem();
-                Skill newSkill = nameToSkill.get(name);
-                if (mySkill == null || !mySkill.equals(newSkill)) {
-                    myCharacter.dropSkill(mySkill);
-                    myCharacter.initSkill(newSkill);
-                    mySkill = newSkill;
-                }
-                container.updateCalculables();
-            }
-        });
-    }
+		choice.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				String name = (String) e.getItem();
+				Skill newSkill = nameToSkill.get(name);
+				boolean changedAvaibleSkills = false;
+				if (mySkill == null || !mySkill.equals(newSkill)) {
+					if (mySkill != null) {
+						avaibleSkills.add(mySkill.getName());
+						changedAvaibleSkills = true;
+					}
+					myCharacter.dropSkill(mySkill);
+					myCharacter.initSkill(newSkill);
+					mySkill = newSkill;
+				}
+				if (!name.equals(Skill.DEFAULT_ID)) {
+					avaibleSkills.remove(name);
+					changedAvaibleSkills = true;
+				}
+				if (changedAvaibleSkills) {
+					Collections.sort(avaibleSkills);
+				}
+				container.updateCalculables();
+			}
+		});
+	}
 
-    public Skill getSkill() {
-        return mySkill;
-    }
+	public Skill getSkill() {
+		return mySkill;
+	}
 
-    public void attach(ScrollableComponent container){
-        super.attach(container);
-        container.add(choice);
-    }
+	public void attach(ScrollableComponent container) {
+		super.attach(container);
+		container.add(choice);
+	}
 }
